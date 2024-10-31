213fc38d6f21232ba7907e01ede0479033e64458
rim bourogaa <rimobourogaa@gmil.com>
2024-10-31 14:07:13 +0100

    second commit

---
+++ pages/profil.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_const_constructors, camel_case_types
+
+import 'package:flutter/material.dart';
+
+class profil extends StatefulWidget {
+  const profil({super.key});
+
+  @override
+  State<profil> createState() => _profilState();
+}
+
+class _profilState extends State<profil> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+children: [
+  Container(
+    margin: EdgeInsets.all(10),
+    child: Row(
+    ),
+  )
+],
+
+
+
+
+     );
+  }
+}
